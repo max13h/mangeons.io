a36c7bec0018c9b4afe59588ed73ee7f782c357f
maxime hmae <max.hmae@gmail.com>
2023-08-28 15:53:34 +0200

    minor input styling

--- pages/login.vue
+++ pages/login.vue
@@ -5,10 +5,10 @@
     </h1>
     <form action="" method="get" class="flex flex-col">
       <label for="email">Email</label>
-      <input id="email" type="email" name="email" class="mb-2 p-1 border-2 border-secondary rounded-lg" autofocus>
+      <input id="email" type="email" name="email" class="mb-2 p-1 rounded-lg border-2 input-text" autofocus>
       <label for="password">Password</label>
-      <input id="password" type="password" name="password" class="mb-4 p-1 border-2 border-secondary rounded-lg">
-      <input type="submit" value="Se connecter" class="bg-secondary text-light py-1 rounded-lg">
+      <input id="password" type="password" name="password" class="mb-4 p-1 rounded-lg input-text">
+      <input type="submit" value="Se connecter" class="bg-secondary text-light py-1 rounded-lg on-click cursor-pointer">
     </form>
     <NuxtLink to="/register" class="text-secondary p-3 flex justify-center">
       Créer un compte
--- pages/register.vue
+++ pages/register.vue
@@ -5,11 +5,11 @@
     </h1>
     <form action="" method="get" class="flex flex-col">
       <label for="email">Email</label>
-      <input id="email" type="email" name="email" class="mb-2 p-1 border-2 border-secondary rounded-lg" autofocus>
+      <input id="email" type="email" name="email" class="mb-2 p-1 border-2 border-secondary rounded-lg input-text" autofocus>
       <label for="password">Mot de passe</label>
-      <input id="password" type="password" name="password" class="mb-4 p-1 border-2 border-secondary rounded-lg">
+      <input id="password" type="password" name="password" class="mb-4 p-1 border-2 border-secondary rounded-lg input-text">
       <label for="confirm-password">Confirmer le mot de passe</label>
-      <input id="confirm-password" type="password" name="confirm-password" class="mb-4 p-1 border-2 border-secondary rounded-lg">
+      <input id="confirm-password" type="password" name="confirm-password" class="mb-4 p-1 border-2 border-secondary rounded-lg input-text">
       <input type="submit" value="S'inscrire" class="bg-secondary text-light py-1 rounded-lg">
     </form>
     <NuxtLink to="/login" class="text-secondary p-3 flex justify-center">
