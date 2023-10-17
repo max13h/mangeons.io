1b4d936163b8b53cfefccc6ddd059fd8e84fd723
Maxime Hmae <max.hmae@gmail.com>
2023-10-17 15:54:39 +0200

    v1 of saving data recipe in supabase

--- composables/newRecipe.ts
+++ composables/newRecipe.ts
@@ -12,6 +12,57 @@ export const useAddIngredientsModal = async () => {
   newRecipeStore.storeAreas = storeAreas
 }
 
-export const useSaveNewRecipe = () => {
+export const useSaveNewRecipe = async () => {
   const newRecipeStore = useNewRecipeStore()
+  const supabase: any = useSupabaseClient()
+  const userAuth = useSupabaseUser()
+
+  const { data: userId } = await supabase
+    .from("users")
+    .select("id")
+    .eq("user_id", userAuth.value.id)
+
+  const { data: recipeData, error: recipeError } = await supabase
+    .from("recipes")
+    .insert([
+      {
+        name: newRecipeStore.name.value,
+        description: newRecipeStore.description.value,
+        content: newRecipeStore.content.value,
+        author: userId[0].id,
+        cooking_time: "1 hour"
+      }
+    ])
+    .select()
+
+  if (recipeError) {
+    return console.log("An error occures during the recipe creation")
+  }
+
+  let errorDuringIngredientInsert = false
+
+  newRecipeStore.ingredients.forEach( async (obj) => {
+    const { data: alimentaryProductRecipeData, error: alimentaryProductsRecipesError } = await supabase
+      .from("alimentary_products_recipes")
+      .insert([
+        {
+          recipe_id: recipeData[0].id,
+          alimentary_product_id: obj.details.id,
+          quantity: obj.quantity,
+          units: obj.units
+        }
+      ])
+      .select()
+
+    if (alimentaryProductsRecipesError) {
+      errorDuringIngredientInsert = true
+      console.log(alimentaryProductsRecipesError)
+    }
+  })
+
+  if (errorDuringIngredientInsert || recipeError) {
+    console.log("errorrrr")
+  } else if (recipeData && !errorDuringIngredientInsert) {
+    return navigateTo("/recettes")
+  }
 }
