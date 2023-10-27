f939939e819c5fa2374e653de5ab758026a157a5
Maxime Hmae <max.hmae@gmail.com>
2023-10-27 11:57:00 +0200

    adding sortablejs to content page of recipe/new

--- components/NewRecipe/Content.vue
+++ components/NewRecipe/Content.vue
@@ -3,26 +3,45 @@
     <h2 class="text-xl mb-4 min-h-[56px]">
       Décrivrez <span class="underline">étape</span> par <span class="underline">étape</span> comment réussir votre recette 🔪
     </h2>
-    <FormTextArea
-      :model="props.content"
-      type="text"
-      name="content"
-      :error="props.errors.content"
-      :placeholder="placeholder"
-      :disable-tab="true"
-      class="min-h-[300px] mb-4"
-    >
-    </FormTextArea>
+
+    <div ref="el" class="overflow-hidden">
+      <div v-for="(step, index) in steps" :key="index" :data-value="step" class="flex items-center border-dashed border-2 border-secondary rounded-xl bg-slate-200 p-4 mb-2">
+        <textarea v-model="steps[index]" type="text" class="border-white me-4" />
+        <i class="ri-draggable text-2xl drag-element" />
+      </div>
+    </div>
+    <button class="btn-secondary" @click="addStep">Add Step</button>
+    <button class="btn-secondary" @click="view">View value</button>
   </div>
 </template>
 
 <script setup lang="ts">
-const props = defineProps<{
-  content: any;
-  errors: any
-}>()
+import Sortable from "sortablejs"
 
-const placeholder = "1. Préchauffer le four à 180°C \n2. .."
+const el = ref(null)
+
+onMounted(() => {
+  el.value.focus()
+
+  const sortable = new Sortable(el.value, {
+    animation: 150,
+    ghostClass: "blue-background-class",
+    dataIdAttr: "data-value",
+    handle: ".drag-element",
+    onUpdate: function () {
+      console.log(this.toArray())
+    }
+  })
+})
+
+const steps = ref([""])
+
+const addStep = () => {
+  steps.value.push("")
+}
+const view = () => {
+  console.log(steps)
+}
 </script>
 
 <style scoped>
