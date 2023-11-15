f47774846f3edfa963e741abba292341fe954f5e
Maxime Hmae <max.hmae@gmail.com>
2023-11-15 11:46:51 +0100

    feat: add landing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,21 @@
 <template>
-  <div>
-    <NuxtLink to="/login" class="">
-      Sign In
-    </NuxtLink>
+  <div class="h-screen bg-light flex flex-col items-center p-4 text-center">
+    <div class="flex-grow flex flex-col items-center p-4">
+      <NuxtImg src="/logos/LONG_LIGHT_LOGO.svg" class="w-[66%] my-8" />
+      <h1 class="">
+        Optimiser la préparation de vos repas et simplifier vos courses !
+      </h1>
+      <p class="my-8">
+        ⚠️ Site en construction ⚠️
+      </p>
+    </div>
+    <div class="flex flex-col items-center w-full md:w-72">
+      <NuxtLink to="/register" class="btn-secondary w-full">
+        Inscription
+      </NuxtLink>
+      <NuxtLink to="/login" class="btn-outline-secondary mt-4 w-full">
+        Connexion
+      </NuxtLink>
+    </div>
   </div>
 </template>
