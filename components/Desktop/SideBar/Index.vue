d49ec899429f06342ea27c2e4fcb5537785d0ee4
Maxime Hmae <max.hmae@gmail.com>
2023-11-16 22:48:07 +0100

    feat: adding content to sidebar

--- components/Desktop/SideBar/Index.vue
+++ components/Desktop/SideBar/Index.vue
@@ -3,7 +3,7 @@
     class="transform fixed top-0 left-16 w-96 h-dynamic-screen bg-light ease-in-out transition-all duration-300 z-30"
     :class="pageStore.isSideBarOpen ? 'translate-x-0' : '-translate-x-full'"
   >
-    <MobileSideBarContent />
+    <DesktopSideBarContent />
   </aside>
   <div v-if="pageStore.isSideBarOpen" class="overlay absolute bg-dark opacity-50 w-screen h-dynamic-screen" @click="useToggleSideBar" />
 </template>
