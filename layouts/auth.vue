8617e1973202f0591c331efa3ca6f95463f7c587
Maxime Hmae <max.hmae@gmail.com>
2023-11-28 15:38:47 +0100

    fix: height issue on auth layout

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="h-dynamic-screen bg-green-200 flex justify-center items-center relative">
-    <NuxtLink to="/" class="absolute top-8 left-1/2 -translate-x-1/2 w-[200px]">
+  <div class="min-h-dynamic-screen bg-green-200 flex flex-col justify-center items-center relative p-4 overflow-hidden">
+    <NuxtLink to="/" class="my-4">
       <NuxtImg src="/logos/LONG_GREEN_LOGO.svg" alt="mangeons.io logo" />
     </NuxtLink>
-    <div class="card md:w-[600px] bg-light p-8 mx-4">
-      <slot />
+    <div class="grow flex justify-center items-center">
+      <div class="card md:w-[600px] bg-light p-8">
+        <slot />
+      </div>
     </div>
     <NoticeContainer />
   </div>
