ee2af41762c75107c976cfe9addfbb4ec7d3eb66
Maxime Hmae <max.hmae@gmail.com>
2023-12-10 19:08:15 +0100

    fix: apply changes from /recettes/new to /recettes/[id]/edit

--- pages/app/recettes/[id]/edit.vue
+++ pages/app/recettes/[id]/edit.vue
@@ -10,20 +10,21 @@
         </li>
       </ol>
     </Teleport>
-    <div class="flex flex-col items-center h-full w-full overflow-x-hidden flex-grow py-4">
-      <p class="mb-4 text-center rounded-md bg-primary-100 p-2">
-        Renseignez les infomations concernant votre nouvelle recette !
-      </p>
-
+    <div class="flex flex-col items-center h-full w-full overflow-x-hidden flex-grow pt-4 pl-4 pr-4">
       <div class="w-full overflow-x-hidden flex-grow ">
         <swiper
-          v-if="recipe"
+          v-if="recipe && recipe.name"
           class="h-full"
           :allow-touch-move="false"
           :space-between="30"
           :navigation="{
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
+          }"
+          :pagination="{
+            el: '.swiper-pagination-el',
+            type: 'bullets',
+            bulletActiveClass: 'swiper-pagination-active-bullet'
           }"
           @reach-end="reachEnd = true"
         >
@@ -42,17 +43,20 @@
         </swiper>
       </div>
     </div>
-    <div class="flex justify-between w-full min-w-[100px] border border-slate-100 shadow-xl z-20 p-4 relative bg-light rounded-3xl ">
-      <button type="button" class="swiper-button-prev page-btn">
-        <i class="ri-arrow-left-double-line" />
-      </button>
-      <button v-if="reachEnd" type="button" class="btn-primary border-none flex items-center" @click="onSubmit">
-        Enregistrer
-        <i class="ri-save-3-line inline-block text-2xl w-8" />
-      </button>
-      <button type="button" class="swiper-button-next page-btn">
-        <i class="ri-arrow-right-double-line" />
-      </button>
+    <div class="flex flex-col items-center w-full">
+      <span class="swiper-pagination-el w-full flex justify-center my-1" />
+      <div class="flex justify-between w-full px-4 pb-4 pt-2 relative">
+        <button type="button" class="swiper-button-prev page-btn btn-outline-primary">
+          <Icon name="fluent:chevron-double-left-16-filled" size="1.8rem" />
+        </button>
+        <button v-if="reachEnd" type="button" class="flex items-center btn-primary shadow-md" @click="onSubmit">
+          <span class="hidden-under-320 me-2">Enregistrer</span>
+          <Icon name="fluent:save-16-filled" size="1.5rem" />
+        </button>
+        <button type="button" class="swiper-button-next page-btn btn-outline-primary">
+          <Icon name="fluent:chevron-double-right-16-filled" size="1.8rem" />
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -76,13 +80,16 @@ const arrayOfErrors: globalThis.Ref<string[]> = ref([])
 const recipe: globalThis.Ref<any> = ref(null)
 
 onMounted(async () => {
-  const { data, error } = await useFetch("/api/recipe/recipe", {
-    query: { id: route.params.id }
+  const { data, error } = await useAsyncData("getRecipe", async () => {
+    const supabase = useSupabaseClient()
+
+    const { data, error } = await supabase.rpc("get_all_data_of_recipe", { recipe_id: route.params.id })
+
+    useHandleSupabaseIssue(data, error)
+    return data[0]
   })
 
-  if (error.value) {
-    throw new Error("Error on fetch")
-  }
+  useHandleFetchError(error)
 
   recipe.value = data.value
   recipeStore.selectedAlimentaryProducts = data.value.alimentary_products
@@ -112,19 +119,17 @@ const onSuccess = async (values: any) => {
     throw new Error(`Error on useFetch => ${JSON.stringify(error)}`)
   }
 }
-const onInvalidSubmit = ({ values, errors }: { values: any, errors: any }) => {
+const onInvalidSubmit = ({ errors }: { errors: any }) => {
   arrayOfErrors.value = Object.values(errors)
   useOpenModal("recipeFormInvalid")
-  console.log(values);
 }
 const onSubmit = handleSubmit(onSuccess, onInvalidSubmit)
 </script>
 
 <style scoped>
 .page-btn {
-  @apply p-2 bg-primary-100 rounded-lg w-16 text-2xl
+  @apply w-fit px-4 shadow-md
 }
-
 .swiper-button-disabled{
     @apply opacity-0
 }
@@ -135,5 +140,11 @@ const onSubmit = handleSubmit(onSuccess, onInvalidSubmit)
 
 .my-shadow {
   box-shadow: 0px 1px 24px 0px rgba(0,0,0,0.45);
+}
+
+@media (max-width: 320px) {
+  .hidden-under-320 {
+    @apply hidden
+  }
 }
 </style>
