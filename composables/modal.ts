09ae344dba65628a18d70762b964a8eeb6d3d632
Maxime Hmae <max.hmae@gmail.com>
2023-12-10 18:35:11 +0100

    feat: add useIsModalOpen composable

--- composables/modal.ts
+++ composables/modal.ts
@@ -1,6 +1,9 @@
-import { useModalStore } from "../stores/modalStore"
-
 export const useOpenModal = (element: string) => {
   const modalStore = useModalStore()
   modalStore.open(element)
+}
+
+export const useIsModalOpen = (element: string) => {
+  const modalStore = useModalStore()
+  return modalStore.whatIsOpen === element
 }
