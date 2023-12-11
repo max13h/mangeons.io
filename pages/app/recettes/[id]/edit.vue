84e022018aad6cfaccd5f0b6edb137a54bbab22f
Maxime Hmae <max.hmae@gmail.com>
2023-12-11 10:47:03 +0100

    refactor: swiper from /recettes/[id]/edit & from package.json

--- pages/app/recettes/[id]/edit.vue
+++ pages/app/recettes/[id]/edit.vue
@@ -10,50 +10,86 @@
         </li>
       </ol>
     </Teleport>
-    <div class="flex flex-col items-center h-full w-full overflow-x-hidden flex-grow pt-4 pl-4 pr-4">
-      <div class="w-full overflow-x-hidden flex-grow ">
-        <swiper
-          v-if="recipe && recipe.name"
-          class="h-full"
-          :allow-touch-move="false"
-          :space-between="30"
-          :navigation="{
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-          }"
-          :pagination="{
-            el: '.swiper-pagination-el',
-            type: 'bullets',
-            bulletActiveClass: 'swiper-pagination-active-bullet'
-          }"
-          @reach-end="reachEnd = true"
-        >
-          <swiper-slide>
-            <RecipeFormNameDescriptionAndCategory :name="recipe.name" :description="recipe.description" :category="recipe.meal_category_id" />
-          </swiper-slide>
-          <swiper-slide>
-            <RecipeFormCookingTimeAndKitchenEquipments :cooking-time="recipe.cooking_time" />
-          </swiper-slide>
-          <swiper-slide>
-            <RecipeFormAlimentaryProducts />
-          </swiper-slide>
-          <swiper-slide>
-            <RecipeFormContent :content="recipe.content"></RecipeFormContent>
-          </swiper-slide>
-        </swiper>
-      </div>
+    <div v-if="recipe && recipe.name" class="flex-grow pt-4 pl-4 pr-4 overflow-y-scroll overflow-x-hidden">
+      <Transition
+        enter-active-class="transition duration-100 ease-out"
+        enter-from-class="-translate-x-full"
+        enter-to-class="translate-x-0"
+        leave-active-class="transition duration-100 ease-in"
+        leave-from-class="translate-x-0"
+        leave-to-class="-translate-x-full"
+      >
+        <RecipeFormNameDescriptionAndCategory v-if="pageNb === 1" :name="recipe.name" :description="recipe.description" :category="recipe.meal_category_id" />
+      </Transition>
+      <Transition
+        enter-active-class="transition duration-100 ease-out"
+        :enter-from-class="goToNext ? 'translate-x-full' : '-translate-x-full'"
+        enter-to-class="translate-x-0"
+        leave-active-class="transition duration-100 ease-in"
+        leave-from-class="translate-x-0"
+        :leave-to-class="goToNext ? '-translate-x-full' : 'translate-x-full'"
+      >
+        <RecipeFormCookingTimeAndKitchenEquipments v-if="pageNb === 2" :cooking-time="recipe.cooking_time" />
+      </Transition>
+      <Transition
+        enter-active-class="transition duration-100 ease-out"
+        :enter-from-class="goToNext ? 'translate-x-full' : '-translate-x-full'"
+        enter-to-class="translate-x-0"
+        leave-active-class="transition duration-100 ease-in"
+        leave-from-class="translate-x-0"
+        :leave-to-class="goToNext ? '-translate-x-full' : 'translate-x-full'"
+      >
+        <RecipeFormAlimentaryProducts v-if="pageNb === 3" />
+      </Transition>
+      <Transition
+        enter-active-class="transition duration-100 ease-out"
+        enter-from-class="translate-x-full"
+        enter-to-class="translate-x-0"
+        leave-active-class="transition duration-100 ease-in"
+        leave-from-class="translate-x-0"
+        leave-to-class="translate-x-full"
+      >
+        <RecipeFormContent v-if="pageNb === 4" :content="recipe.content"></RecipeFormContent>
+      </Transition>
     </div>
     <div class="flex flex-col items-center w-full">
-      <span class="swiper-pagination-el w-full flex justify-center my-1" />
+      <div class="w-full flex justify-center my-1">
+        <span
+          class="pagination-bullet"
+          :class="{ 'pagination-bullt-active': pageNb === 1 }"
+        />
+        <span
+          class="pagination-bullet"
+          :class="{ 'pagination-bullt-active': pageNb === 2 }"
+        />
+        <span
+          class="pagination-bullet"
+          :class="{ 'pagination-bullt-active': pageNb === 3 }"
+        />
+        <span
+          class="pagination-bullet"
+          :class="{ 'pagination-bullt-active': pageNb === 4 }"
+        />
+      </div>
       <div class="flex justify-between w-full px-4 pb-4 pt-2 relative">
-        <button type="button" class="swiper-button-prev page-btn btn-outline-primary">
+        <button
+          type="button"
+          class="page-btn btn-outline-primary"
+          :class="{ 'invisible': pageNb === 1 }"
+          @click="prevPage()"
+        >
           <Icon name="fluent:chevron-double-left-16-filled" size="1.8rem" />
         </button>
         <button v-if="reachEnd" type="button" class="flex items-center btn-primary shadow-md" @click="onSubmit">
           <span class="hidden-under-320 me-2">Enregistrer</span>
           <Icon name="fluent:save-16-filled" size="1.5rem" />
         </button>
-        <button type="button" class="swiper-button-next page-btn btn-outline-primary">
+        <button
+          type="button"
+          class="page-btn btn-outline-primary"
+          :class="{ 'invisible': pageNb === 4 }"
+          @click="nextPage()"
+        >
           <Icon name="fluent:chevron-double-right-16-filled" size="1.8rem" />
         </button>
       </div>
@@ -62,18 +98,53 @@
 </template>
 
 <script setup lang="ts">
-import { register } from "swiper/element/bundle"
-import { Swiper, SwiperSlide } from "swiper/vue"
-import "swiper/css"
-
 definePageMeta({
   layout: "app-deep-focus"
 })
 const recipeStore = useRecipeStore()
 const route = useRoute()
 
-register()
+const pageNb = ref(1)
+const changingPage = ref(false)
+const goToNext = ref(false)
 const reachEnd = ref(false)
+
+const nextPage = () => {
+  const previousPage = pageNb.value
+  if (previousPage === 4 || changingPage.value) {
+    return
+  }
+
+  changingPage.value = true
+
+  goToNext.value = true
+  pageNb.value = 0
+
+  setTimeout(() => {
+    pageNb.value = previousPage + 1
+    changingPage.value = false
+
+    if (pageNb.value === 4) {
+      reachEnd.value = true
+    }
+  }, 150)
+}
+const prevPage = () => {
+  const previousPage = pageNb.value
+  if (previousPage === 1 || changingPage.value) {
+    return
+  }
+
+  changingPage.value = true
+
+  goToNext.value = false
+  pageNb.value = 0
+
+  setTimeout(() => {
+    pageNb.value = previousPage - 1
+    changingPage.value = false
+  }, 150)
+}
 
 const arrayOfErrors: globalThis.Ref<string[]> = ref([])
 
@@ -130,16 +201,17 @@ const onSubmit = handleSubmit(onSuccess, onInvalidSubmit)
 .page-btn {
   @apply w-fit px-4 shadow-md
 }
-.swiper-button-disabled{
-    @apply opacity-0
-}
-
-.swiper-slide {
-  overflow-y: scroll;
-}
 
 .my-shadow {
   box-shadow: 0px 1px 24px 0px rgba(0,0,0,0.45);
+}
+
+.pagination-bullet {
+  @apply w-2 h-2 bg-slate-300 rounded-full mx-1
+}
+
+.pagination-bullt-active {
+  @apply bg-primary
 }
 
 @media (max-width: 320px) {
